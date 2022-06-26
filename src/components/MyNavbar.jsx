7dfb86011fe9ed70e9037536358225b2d996d20b
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-06-26 03:14:50 +0200

    solved exercise 2 and 3

---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,22 @@
+import { Navbar, Nav } from 'react-bootstrap'
+
+
+const MyNavbar = (props) => {
+ 
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">{props.navTitle}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#deets">Home</Nav.Link>
+          <Nav.Link href="#deets">About</Nav.Link>
+          <Nav.Link href="#memes">Browse</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default MyNavbar
+
