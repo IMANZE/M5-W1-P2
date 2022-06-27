e83f463eb5e366cfc55b29e24769946852edd8af
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-06-27 12:36:15 +0200

    solved exercise 5
    Create a Welcome component with a Jumbotron and a subtitle for your shop.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div>
       <MyNavbar navTitle="Ezi Bookstore" />
+     
+      <Welcome subtitle="Welcome to the Ezi BOOK Store" bookSection="Romance Book Section"/>
       <MyFooter/>
     </div>
   );
